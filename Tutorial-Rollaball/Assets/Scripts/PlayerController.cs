56fe5537a178285aba771d3f7d998ddb46371f40
ssangervasi <ssangervasi@colgate.edu>
2015-02-15 13:48:56 -0800

    Player movement physics

---
+++ Tutorial-Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertial = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertial);
+		rigidbody.AddForce (speed*movement*Time.deltaTime);
+
+	}
+}
