5e413c355cdaf546f633a11af7b270291da92c08
Sebastian-Nicole-laptop <vanmeternicole@gmail.com>
2015-02-15 21:54:39 -0800

    Pickup items

---
+++ Tutorial-Rollaball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public float anglePerSecond;
+
+	void Update() {
+		float deltaAngle = (anglePerSecond*Time.deltaTime);
+		transform.Rotate (deltaAngle, deltaAngle, 0);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
